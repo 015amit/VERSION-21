833fc8ceb8a19508fbc315feb2ac24167460f0d2
Nikita <nikitadasmsd@gmail.com>
2021-05-12 21:36:19 +0530

    color

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Nikita Das\Desktop\VERSION\venv
+set VIRTUAL_ENV=C:\Users\Nikita Das\Desktop\VERSION-21\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
