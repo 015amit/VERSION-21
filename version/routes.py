630a16d9a79c915c77843b8a11c409e5b12b3b4b
amit kumar <ak2383977@gmail.com>
2021-05-12 22:56:42 +0530

    title updated

--- version/routes.py
+++ version/routes.py
@@ -3,15 +3,15 @@ from version import app
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title="Home")
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title="About Us")
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', title="Contact Us")
 
 
 
@@ -26,4 +26,4 @@ def teams():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html', title="Page Not Found"), 404
